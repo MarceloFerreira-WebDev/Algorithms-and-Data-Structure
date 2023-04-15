58776f7e9584ec6fcc1c7c2010e3e04ce5495b82
Marcelo Ferreira <marceloferreira.ihu72@gmail.com>
2023-04-15 10:24:28 -0300

    solução 2

--- solutions/solution1.js
+++ solutions/solution1.js
@@ -1,14 +1,20 @@
 const arrayReader = (array) => {
   if (!array) return 'por favor, passe um array para a função';
-  if (typeof array === 'object') return 'por favor, passe um objeto do tipo array para a função';
+  if (!Array.isArray(array)) return 'por favor, passe um objeto do tipo array para a função';
 
   const justLetters = array.filter((element) => typeof element === 'string');
   const justNumbers = array.filter((element) => typeof element === 'number');
-  const highestNumber = Math.max(justNumbers);
+  const highestNumber = Math.max(...justNumbers);
 
   console.log(
     `Array só de letras: ${justLetters}
-    Array só de números: ${justNumbers}
-    Maior número do Array: ${highestNumber}`
+Array só de números: ${justNumbers}
+Maior número do Array: ${highestNumber}`
   );
+
+  return `Array só de letras: ${justLetters}
+Array só de números: ${justNumbers}
+Maior número do Array: ${highestNumber}`
 };
+
+arrayReader(['a', 10, 'b', 'hola', 122, 15]);
